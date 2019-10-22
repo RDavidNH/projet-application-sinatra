dbf77947c699c73531f162ef4192f88ae81099f4
R David N H <rdavidnh@gmail.com>
2019-10-22 09:19:49 +0300

    update

--- lib/controller.rb
+++ lib/controller.rb
@@ -4,19 +4,22 @@ require 'gossip'
 
 class ApplicationController < Sinatra::Base
 
+  # gossips index
   get '/' do
     erb :index, locals: {gossips: Gossip.all}
   end
   
+  # show a specific gossip
   get '/gossip/:id' do
     erb :show, locals: {gossip: Gossip.find(params['id'])}
   end
   
-  
+  # display new gossip form
   get '/gossips/new/' do
     erb :new_gossip
   end
   
+  # save new gossip to DB
   post '/gossips/new/' do
   
     Gossip.new(params['gossip_author'], params['gossip_content']).save
@@ -30,10 +33,13 @@ class ApplicationController < Sinatra::Base
     puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
   end
 
+  # show edit form for a gossip
   get '/gossips/:id/edit' do
     erb :edit_gossip, locals: {id: params['id']}
   end
 
+  # update a specific gossip
+  # TODO
   post '/gossips/:id/edit' do
     puts "Update gossip"
     Gossip.new(params['gossip_author'], params['gossip_content']).update(params['id'])
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,16 +1,19 @@
 require 'csv'
 
 class Gossip
-
+  
   attr_accessor :author, :content
-
+  
   def initialize(author, content)
     @author = author
     @content = content
   end
   
   def self.all
+    # array to store all gossips
     all_gossips = []
+    
+    # read gossip from csv file and store them into an array
     CSV.read("./db/gossip.csv").each do |csv_line|
       all_gossips << Gossip.new(csv_line[0], csv_line[1])
     end
@@ -19,7 +22,7 @@ class Gossip
   end
   
   def self.find(id)
-    
+    # read gossip csv file and return the one with a specific id
     CSV.foreach("./db/gossip.csv").with_index do |row, i|
       if i + 1 == id.to_i
         row << i + 1
@@ -29,6 +32,7 @@ class Gossip
   end
 
   def save
+    # open csv file and append new data 
     CSV.open("./db/gossip.csv", "ab") do |csv|
       csv << [@author, @content]
     end
@@ -36,8 +40,7 @@ class Gossip
 
   def update(id)
     CSV.open("./db/gossip.csv", "ab") do |csv|
-      
-      
+      # todo
     end
   end
 end
